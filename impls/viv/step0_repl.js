9d90121992f352e294049bd57a482f0850556651
Vivek Bisht <99522211+beastvivek@users.noreply.github.com>
2023-05-26 11:03:08 +0530

    Changed process for Step 0: The REPL

--- impls/viv/step0_repl.js
+++ impls/viv/step0_repl.js
@@ -1,16 +1,18 @@
-const { stdin, stdout} = process;
-
-const READ = str => str; 
-const EVAL = str => str; 
-const PRINT = str => str; 
-const rep = str => PRINT(EVAL(READ(str))); 
-
-stdin.setEncoding('utf-8');
-stdout.setEncoding('utf-8');
-
-stdout.write('user> ');
-
-stdin.on('data', chunk => {
-  stdout.write(rep(chunk));
-  stdout.write('user> ');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+
+const READ = str => str;
+const EVAL = str => str;
+const PRINT = str => str;
+
+const rep = str => PRINT(EVAL(READ(str)));
+
+const repl = () =>
+  rl.question('user> ', line => {
+    console.log(rep(line));
+    repl();
+  });
+
+repl();
